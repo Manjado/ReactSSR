c65b4b1c2da19d7e8afa7120211bddfecd5c0fc8
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-06-26 17:57:02 +0200

    add nodemon

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,11 @@ const renderToString = require('react-dom/server').renderToString;
 const Home = require('./client/components/Home').default;
 const app = express();
 
-app.get('/', (req,res) => {});
+app.get('/', (req,res) => {
+   const content = renderToString(<Home />)
+
+    res.send(content);
+});
 
 app.listen(3000, () => {
    console.log('Listening on port 3000');
