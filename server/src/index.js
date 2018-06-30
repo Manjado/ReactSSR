fd221bba7622508657a9d488dc6b5a46841b2dd2
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-06-30 22:07:48 +0200

    add webpack client side

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,20 @@ import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 
 const app = express();
-app.get('/', (req,res) => {
-   const content = renderToString(<Home />)
 
-    res.send(content);
+app.use(express.static('public'));
+app.get('/', (req,res) => {
+    const content = renderToString(<Home />);
+    const html = `        
+        <html>
+            <head></head>
+            <body>
+                <div id="root">${content}</div>
+                <script src="bundle.js"></script>
+            </body>
+        </html>`;
+
+    res.send(html);
 });
 
 app.listen(3000, () => {
